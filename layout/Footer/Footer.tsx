6bbe41dd60d3e5c75f0904c000c5d0e8c89e8d44
M_Ulaonovych <m.ulanovych@gmail.com>
2023-03-05 00:20:59 +0200

    feat:add styles for layout

--- layout/Footer/Footer.tsx
+++ layout/Footer/Footer.tsx
@@ -1,7 +1,18 @@
 import { FooterProps } from './Footer.props';
-import styles from './Header.module.css';
+import styles from './Footer.module.css';
 import cn from 'classnames';
+import { format } from 'date-fns';
 
-export const Footer = ({ ...props }: FooterProps): JSX.Element => {
-	return <div {...props}>Footer</div>;
+export const Footer = ({ className, ...props }: FooterProps): JSX.Element => {
+	return (
+		<footer className={cn(className, styles.footer)} {...props}>
+			<div>OwlTop © {format(new Date(), 'yyyy')} Всі права захищені</div>
+			<a href='#' target='_blank'>
+				Правила і умови користувача
+			</a>
+			<a href='#' target='_blank'>
+				Політика конфединційності
+			</a>
+		</footer>
+	);
 };
